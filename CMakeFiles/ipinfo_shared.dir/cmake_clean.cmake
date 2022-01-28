ac35fe8733edacddcac4861574becd065196b6ed
ownesis <83978267+ownesis@users.noreply.github.com>
2022-01-28 15:04:31 +0100

    Add Cmake file

---
+++ CMakeFiles/ipinfo_shared.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ipinfo_shared.dir/ipinfo.c.o"
+  "CMakeFiles/ipinfo_shared.dir/ipinfo.c.o.d"
+  "libipinfo_shared.pdb"
+  "libipinfo_shared.so"
+  "libipinfo_shared.so.0.1.0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ipinfo_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
