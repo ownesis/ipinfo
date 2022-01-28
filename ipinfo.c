e6aeddeda8e3fb6332781e2767a8f38b19be636a
ownesis <83978267+ownesis@users.noreply.github.com>
2022-01-28 14:48:52 +0100

    First commit

---
+++ ipinfo.c
@@ -0,0 +1,222 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+#include <json-c/json.h>
+#include "ipinfo.h"
+
+#define FREE_IF_NONULL(p) if ((p)) free((p))
+
+#define CURL_ERR(f) if ((f) != CURLE_OK) 
+
+#define JSON_GET_STR(j, k, s)                               \
+    do {                                                    \
+        json_object *_tmp = NULL;                           \
+        if (!json_object_object_get_ex((j), #k, &_tmp)) {   \
+            free((s));                                      \
+            json_object_put((j));                           \
+            return IPINFO_JSON_ERR;                         \
+        }                                                   \
+        (s)->k = strdup(json_object_get_string(_tmp));      \
+        if (!(s)->k) {                                      \
+            free((s));                                      \
+            json_object_put((j));                           \
+            return IPINFO_MEM_ERR;                          \
+        }                                                   \
+    } while (0)
+
+#define JSON_GET_OBJ(t, j, k, s)                            \
+    do {                                                    \
+        json_object *_tmp = NULL;                           \
+        if (!json_object_object_get_ex((j), #k, &_tmp)) {   \
+            free((s));                                      \
+            json_object_put((j));                           \
+            return IPINFO_MEM_ERR;                          \
+        }                                                   \
+        (s)->k = json_object_get_##t(_tmp);                 \
+    } while (0)
+
+
+struct Mem {
+    char *ptr;
+    size_t size;
+};
+
+static size_t _write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+    size_t total_size = (size * nmemb);
+    struct Mem *chunk = (struct Mem *)userdata;
+    char *tmp = realloc(chunk->ptr, chunk->size+total_size+1);
+
+    if (!tmp)
+        return 0;
+
+    chunk->ptr = tmp;
+    memcpy(chunk->ptr+chunk->size, ptr, total_size);
+    chunk->size += total_size;
+
+    return total_size;
+}
+
+void ipinfo_free(struct IPInfo *info) {
+    if (!info)
+        return;
+
+    FREE_IF_NONULL(info->ip);
+    FREE_IF_NONULL(info->version);
+    FREE_IF_NONULL(info->city);
+    FREE_IF_NONULL(info->region);
+    FREE_IF_NONULL(info->region_code);
+    FREE_IF_NONULL(info->country);
+    FREE_IF_NONULL(info->country_name);
+    FREE_IF_NONULL(info->country_code);
+    FREE_IF_NONULL(info->country_code_iso3);
+    FREE_IF_NONULL(info->country_capital);
+    FREE_IF_NONULL(info->country_tld);
+    FREE_IF_NONULL(info->continent_code);
+    FREE_IF_NONULL(info->postal);
+    FREE_IF_NONULL(info->timezone);
+    FREE_IF_NONULL(info->utc_offset);
+    FREE_IF_NONULL(info->country_calling_code);
+    FREE_IF_NONULL(info->currency);
+    FREE_IF_NONULL(info->currency_name);
+    FREE_IF_NONULL(info->languages);
+    FREE_IF_NONULL(info->asn);
+    FREE_IF_NONULL(info->org);
+
+    free(info);
+}
+
+int ipinfo_get(struct IPInfo **info) {
+    CURL *curl_handle;
+    CURLcode res;
+    struct Mem chunk;
+    struct json_object *jobj;
+    struct json_object *loc_obj;
+    _Bool test;
+    long code = 0;
+
+    *info = calloc(1, sizeof(struct IPInfo));
+    if (!(*info))
+        return IPINFO_MEM_ERR;
+
+    chunk.ptr = calloc(1, 1);
+
+    if (!chunk.ptr) {
+        free(*info);
+
+        return IPINFO_MEM_ERR;
+    }
+
+    chunk.size = 0;
+
+    curl_handle = curl_easy_init();
+
+    if (!curl_handle) {
+        free(*info);
+        free(chunk.ptr);
+
+        return IPINFO_CURL_ERR;
+    }
+
+    CURL_ERR(curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "ipinfo")) {
+        free(*info);
+        free(chunk.ptr);
+
+        return IPINFO_CURL_ERR;
+    }
+
+    CURL_ERR(curl_easy_setopt(curl_handle, CURLOPT_URL, "https://ipapi.co/json")) {
+        free(*info);
+        free(chunk.ptr);
+        curl_easy_cleanup(curl_handle);
+
+        return IPINFO_CURL_ERR;
+    }
+    
+    CURL_ERR(curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, _write_callback)) {
+        free(*info);
+        free(chunk.ptr);
+        curl_easy_cleanup(curl_handle);
+
+        return IPINFO_CURL_ERR;
+    }
+    
+    CURL_ERR(curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk)) {
+        free(*info);
+        free(chunk.ptr);
+        curl_easy_cleanup(curl_handle);
+        
+        return IPINFO_CURL_ERR;
+    }
+
+    CURL_ERR(curl_easy_perform(curl_handle)) {
+        free(*info);
+        free(chunk.ptr);
+        curl_easy_cleanup(curl_handle);
+
+        return IPINFO_CURL_ERR;
+    }
+
+    CURL_ERR(curl_easy_getinfo(curl_handle, CURLINFO_RESPONSE_CODE, &code)) {
+        free(*info);
+        free(chunk.ptr);
+        curl_easy_cleanup(curl_handle);
+
+        return IPINFO_CURL_ERR;
+    }
+
+    curl_easy_cleanup(curl_handle);
+
+    if (code != 200) {
+        free(*info);
+        free(chunk.ptr);
+
+        return IPINFO_CURL_ERR;
+    }
+
+    chunk.ptr[chunk.size] = 0;
+
+    jobj = json_tokener_parse(chunk.ptr);
+
+    free(chunk.ptr);
+
+    if (!jobj) {
+        free(*info);
+
+        return IPINFO_JSON_ERR;
+    }
+
+    JSON_GET_STR(jobj, ip, *info);
+    JSON_GET_STR(jobj, version, *info);
+    JSON_GET_STR(jobj, city, *info);
+    JSON_GET_STR(jobj, region, *info);
+    JSON_GET_STR(jobj, region_code, *info);
+    JSON_GET_STR(jobj, country, *info);
+    JSON_GET_STR(jobj, country_name, *info);
+    JSON_GET_STR(jobj, country_code, *info);
+    JSON_GET_STR(jobj, country_code_iso3, *info);
+    JSON_GET_STR(jobj, country_capital, *info);
+    JSON_GET_STR(jobj, country_tld, *info);
+    JSON_GET_STR(jobj, continent_code, *info);
+    JSON_GET_STR(jobj, postal, *info);
+    JSON_GET_STR(jobj, timezone, *info);
+    JSON_GET_STR(jobj, utc_offset, *info);
+    JSON_GET_STR(jobj, country_calling_code, *info);
+    JSON_GET_STR(jobj, currency, *info);
+    JSON_GET_STR(jobj, currency_name, *info);
+    JSON_GET_STR(jobj, languages, *info);
+    JSON_GET_STR(jobj, asn, *info);
+    JSON_GET_STR(jobj, org, *info);
+
+    JSON_GET_OBJ(int64, jobj, country_population, *info);
+
+    JSON_GET_OBJ(double, jobj, latitude, *info);
+    JSON_GET_OBJ(double, jobj, longitude, *info);
+    JSON_GET_OBJ(double, jobj, country_area, *info);
+
+    JSON_GET_OBJ(boolean, jobj, in_eu, *info);
+
+    json_object_put(jobj);
+
+    return IPINFO_OK;
+}
---
+++ ipinfo.h
@@ -0,0 +1,43 @@
+#ifndef __IP_INFO_H__
+#define __IP_INFO_H__
+
+struct IPInfo {
+    char *ip;
+    char *version;
+    char *city;
+    char *region;
+    char *region_code;
+    char *country;
+    char *country_name;
+    char *country_code;
+    char *country_code_iso3;
+    char *country_capital;
+    char *country_tld;
+    char *continent_code;
+    char *postal;
+    double latitude;
+    double longitude;
+    char *timezone;
+    char *utc_offset;
+    char *country_calling_code;
+    char *currency;
+    char *currency_name;
+    char *languages;
+    double country_area;
+    size_t country_population;
+    char *asn;
+    char *org;
+    _Bool in_eu;
+};
+
+enum _E_IPInfo_ {
+    IPINFO_OK = 0,
+    IPINFO_MEM_ERR,
+    IPINFO_JSON_ERR,
+    IPINFO_CURL_ERR
+};
+
+int ipinfo_get(struct IPInfo **info);
+void ipinfo_free(struct IPInfo *info);
+
+#endif /* __IP_INFO_H__ */
